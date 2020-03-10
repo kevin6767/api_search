5b4aa5683e55c036e8e04797586be815274f8dfe
Matthew Marcus <lurchbulldog@gmail.com>
2020-03-10 10:26:08 -0500

    changes promise effect to use async/await

--- src/components/HeroDetail.js
+++ src/components/HeroDetail.js
@@ -3,19 +3,15 @@ import "../App.css"
 import {fetchHeroDetail} from './services/HeroDetail.services'
 import { useParams } from 'react-router-dom'
 
+const fetchAndSetHeroeDetail = async (heroId, setHeroDetail) => setHeroDetail(await fetchHeroDetail(heroId))
 
 function HeroDetail() {
     const { id } = useParams()
     const [hero, setHero] = useState({})
 
     useEffect(() => {
-         fetchHeroDetail(id).then(hero => {
-            setHero(hero);
-
-        });
+         fetchAndSetHeroeDetail(id, setHero)
     }, [id]);
-
-
 
     return(
         <div>
